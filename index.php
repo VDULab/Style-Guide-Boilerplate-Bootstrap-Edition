660a14f4bc5c1fe4c74470355934086b9690e94b
Luca Pomero <lpomero@vdu.it>
2017-09-26 12:30:00 +0200

    Split bootstrap css

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <meta name="viewport" content="width=device-width">
 <!-- Style Guide Boilerplate Styles -->
 <link rel="stylesheet" href="css/styleguide.css">
-<!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+<link rel="stylesheet" href="/sites/all/themes/agora_bootstrap/css/bootstrap.css">
 
 <!-- Replace below stylesheet with your own stylesheet -->
 <link rel="stylesheet" href="/sites/all/themes/agora_bootstrap/css/style.css">
